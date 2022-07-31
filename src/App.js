27c3190392b91efbb24dd5ea832ec3cec59a2c26
John Patrick Resurreccion <jresurreccion@isbx.com>
2022-07-31 12:32:12 +0800

    Tech-197: Design completed

--- src/App.js
+++ src/App.js
@@ -5,13 +5,10 @@ import {
   Grid,
   List,
   ListItem,
-  IconButton,
   Box,
-  makeStyles,
-  Button,
 } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import CachedIcon from "@mui/icons-material/Cached";
+import Content from "./components/Content";
+import { SiReact } from "react-icons/si";
 
 // const useStyles = makeStyles({
 //   content: {
@@ -28,35 +25,64 @@ function App() {
   // const classes = useStyles();
   return (
     <React.Fragment>
-      <Box>
-        <Typography>TOUROFHEROES</Typography>
+      <Box sx={{ backgroundColor: "#292929", padding: "15px 0" }}>
+        <Container>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Typography sx={{ color: "#59cdf4", fontSize: 20, marginRight: 2 }}>
+              <SiReact />
+            </Typography>
+            <Typography
+              sx={{
+                color: "white",
+                textTransform: "uppercase",
+                fontWeight: "700",
+              }}
+              component="p"
+            >
+              tour
+              <Typography
+                component="span"
+                sx={{ color: "#c0c0c0", fontWeight: "700" }}
+              >
+                of
+              </Typography>
+              <Typography
+                component="span"
+                sx={{ color: "#59cdf4", fontWeight: "700" }}
+              >
+                heroes
+              </Typography>
+            </Typography>
+          </Box>
+        </Container>
       </Box>
-      <Container>
-        <Grid container>
-          <Grid item lg={2}>
-            <Typography variant="body2">MENU</Typography>
+      <Container sx={{ marginTop: 5 }}>
+        <Grid container spacing={2}>
+          <Grid item sm={3}>
+            <Typography
+              variant="body2"
+              color="GrayText"
+              gutterBottom
+              sx={{ fontSize: 12, letterSpacing: 1 }}
+            >
+              MENU
+            </Typography>
             <List>
-              <ListItem>Heroes</ListItem>
+              <ListItem
+                sx={{
+                  backgroundColor: "#22a5e2",
+                  color: "white",
+                  borderRadius: 1,
+                }}
+              >
+                Heroes
+              </ListItem>
               <ListItem>Villains</ListItem>
               <ListItem>About</ListItem>
             </List>
           </Grid>
-          <Grid item lg={10}>
-            <Grid container>
-              <Grid item>
-                <Typography>HEROES</Typography>
-              </Grid>
-              <Grid item>
-                <IconButton>
-                  <AddIcon variant="secondary" fontSize="small" />
-                </IconButton>
-              </Grid>
-              <Grid item>
-                <IconButton>
-                  <CachedIcon variant="secondary" fontSize="small" />
-                </IconButton>
-              </Grid>
-            </Grid>
+          <Grid item sm={9}>
+            <Content />
           </Grid>
         </Grid>
       </Container>
---
+++ src/components/Content.js
@@ -0,0 +1,235 @@
+import React from "react";
+import AddIcon from "@mui/icons-material/Add";
+import CachedIcon from "@mui/icons-material/Cached";
+import {
+  Typography,
+  Grid,
+  IconButton,
+  Button,
+  Card,
+  CardContent,
+  CardActions,
+  Box,
+} from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import DeleteIcon from "@mui/icons-material/Delete";
+import BorderColorIcon from "@mui/icons-material/BorderColor";
+
+const useStyles = makeStyles({
+  btn: {
+    width: "100%",
+  },
+});
+
+const Content = () => {
+  const classes = useStyles();
+  return (
+    <Box>
+      <Grid container sx={{ alignItems: "center", marginBottom: 2 }}>
+        <Grid item>
+          <Typography
+            variant="h5"
+            sx={{
+              borderLeft: "15px solid #22a5e2",
+              borderBottom: "2px solid #22a5e2",
+              padding: "2px 10px",
+              fontWeight: "bold",
+              color: "#434343",
+            }}
+          >
+            HEROES
+          </Typography>
+        </Grid>
+        <Grid item>
+          <IconButton>
+            <AddIcon variant="secondary" fontSize="small" />
+          </IconButton>
+        </Grid>
+        <Grid item>
+          <IconButton>
+            <CachedIcon variant="secondary" fontSize="small" />
+          </IconButton>
+        </Grid>
+      </Grid>
+
+      <Card
+        variant="outlined"
+        sx={{
+          backgroundColor: "#f9f9f9",
+          marginBottom: 3,
+          borderRadius: 0,
+          paddingTop: 1,
+          boxShadow: "1px 1px 1px rgba(0, 0, 0, .1)",
+        }}
+      >
+        <CardContent>
+          <Typography gutterBottom variant="h5" sx={{ color: "#1b1b1b" }}>
+            John Patrick Resurreccion
+          </Typography>
+          <Typography
+            sx={{ fontSize: 14, color: "#8e8e8e" }}
+            color="text.secondary"
+          >
+            Word of the Day
+          </Typography>
+        </CardContent>
+        <CardActions
+          sx={{
+            padding: 0,
+            backgroundColor: "white",
+            borderTop: "1px solid #cccccc",
+          }}
+        >
+          <Button
+            className={classes.btn}
+            sx={{
+              marginLeft: 0,
+              borderRadius: 0,
+              color: "rgba(0, 0, 0, .6)",
+              textTransform: "capitalize",
+            }}
+            startIcon={<DeleteIcon />}
+          >
+            Delete
+          </Button>
+          <Button
+            className={classes.btn}
+            sx={{
+              marginLeft: 0,
+              borderRadius: 0,
+              color: "rgba(0, 0, 0, .6)",
+              textTransform: "capitalize",
+              "&:not(:first-of-type)": {
+                marginLeft: 0,
+                borderLeft: "1px solid #cccccc",
+              },
+            }}
+            startIcon={<BorderColorIcon />}
+          >
+            Edit
+          </Button>
+        </CardActions>
+      </Card>
+
+      <Card
+        variant="outlined"
+        sx={{
+          backgroundColor: "#f9f9f9",
+          marginBottom: 3,
+          borderRadius: 0,
+          paddingTop: 1,
+          boxShadow: "1px 1px 1px rgba(0, 0, 0, .1)",
+        }}
+      >
+        <CardContent>
+          <Typography gutterBottom variant="h5" sx={{ color: "#1b1b1b" }}>
+            John Patrick Resurreccion
+          </Typography>
+          <Typography
+            sx={{ fontSize: 14, color: "#8e8e8e" }}
+            color="text.secondary"
+          >
+            Word of the Day
+          </Typography>
+        </CardContent>
+        <CardActions
+          sx={{
+            padding: 0,
+            backgroundColor: "white",
+            borderTop: "1px solid #cccccc",
+          }}
+        >
+          <Button
+            className={classes.btn}
+            sx={{
+              marginLeft: 0,
+              borderRadius: 0,
+              color: "rgba(0, 0, 0, .6)",
+              textTransform: "capitalize",
+            }}
+            startIcon={<DeleteIcon />}
+          >
+            Delete
+          </Button>
+          <Button
+            className={classes.btn}
+            sx={{
+              marginLeft: 0,
+              borderRadius: 0,
+              color: "rgba(0, 0, 0, .6)",
+              textTransform: "capitalize",
+              "&:not(:first-of-type)": {
+                marginLeft: 0,
+                borderLeft: "1px solid #cccccc",
+              },
+            }}
+            startIcon={<BorderColorIcon />}
+          >
+            Edit
+          </Button>
+        </CardActions>
+      </Card>
+
+      <Card
+        variant="outlined"
+        sx={{
+          backgroundColor: "#f9f9f9",
+          marginBottom: 3,
+          borderRadius: 0,
+          paddingTop: 1,
+          boxShadow: "1px 1px 1px rgba(0, 0, 0, .1)",
+        }}
+      >
+        <CardContent>
+          <Typography gutterBottom variant="h5" sx={{ color: "#1b1b1b" }}>
+            John Patrick Resurreccion
+          </Typography>
+          <Typography
+            sx={{ fontSize: 14, color: "#8e8e8e" }}
+            color="text.secondary"
+          >
+            Word of the Day
+          </Typography>
+        </CardContent>
+        <CardActions
+          sx={{
+            padding: 0,
+            backgroundColor: "white",
+            borderTop: "1px solid #cccccc",
+          }}
+        >
+          <Button
+            className={classes.btn}
+            sx={{
+              marginLeft: 0,
+              borderRadius: 0,
+              color: "rgba(0, 0, 0, .6)",
+              textTransform: "capitalize",
+            }}
+            startIcon={<DeleteIcon />}
+          >
+            Delete
+          </Button>
+          <Button
+            className={classes.btn}
+            sx={{
+              marginLeft: 0,
+              borderRadius: 0,
+              color: "rgba(0, 0, 0, .6)",
+              textTransform: "capitalize",
+              "&:not(:first-of-type)": {
+                marginLeft: 0,
+                borderLeft: "1px solid #cccccc",
+              },
+            }}
+            startIcon={<BorderColorIcon />}
+          >
+            Edit
+          </Button>
+        </CardActions>
+      </Card>
+    </Box>
+  );
+};
+
+export default Content;
